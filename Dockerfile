96ff7bbb4c6c890142ccf5af8c14e616a4aa7917
vp15591 <vp15591@mail.com>
2019-04-17 14:20:41 +0530

    Updated the code to use env variables

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM python:3.5-alpine
 RUN apk add build-base python-dev py-pip jpeg-dev zlib-dev
-ENV LIBRARY_PATH=/lib:/usr/lib
+ENV LIBRARY_PATH=/lib:/usr/lib \
+    GRAFANA_URL="http://prom-grafana:80" \
+    GRAFANA_USER="admin" \
+    GRAFANA_PASSWORD="prom-operator" \
+    SLACK_TOKEN="xoxb-573662986324-599506195031-oDP9ZSLQjFzrThZfQVEdgsml" \
+    SLACK_CHANNEL="#kubernetes-bot"
 RUN mkdir -p app
 COPY render.py /app
 COPY requirements.txt /app
--- render.py
+++ render.py
@@ -1,41 +1,44 @@
 import requests
+from requests.auth import HTTPBasicAuth
 import shutil
 import datetime
+import os
 from reportlab.lib.enums import TA_JUSTIFY
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
 
+SlackToken = os.environ.get("SLACK_TOKEN", "xoxb-573662986324-599506195031-oDP9ZSLQjFzrThZfQVEdgsml")
+BASE_URL = os.environ.get('GRAFANA_URL', "http://prom-grafana:80")
+SlackChannels = os.environ.get("SLACK_CHANNEL", "#kubernetes-bot")
+GRAFANA_USER = os.environ.get("GRAFANA_USER", "admin")
+GRAFANA_PASSWORD = os.environ.get("GRAFANA_PASSWORD", "prom-operator")
+
+GET_DASHBOARD_URL = BASE_URL + "/api/search?folderIds=0&query=&starred=false"
+
 doc = SimpleDocTemplate("Report.pdf", pagesize=letter,
                         rightMargin=72, leftMargin=72,
                         topMargin=72, bottomMargin=18)
 Story = []
-styles=getSampleStyleSheet()
+styles = getSampleStyleSheet()
 styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
 image_list = []
 
-
-SlackToken= "xoxb-573662986324-599506195031-oDP9ZSLQjFzrThZfQVEdgsml"
-start_date = (datetime.datetime.now() - datetime.timedelta(minutes=15)).timestamp()
-end_date = datetime.datetime.now().timestamp()
-
-BASE_URL = "http://prom-grafana:80"
-GET_DASHBOARD_URL = BASE_URL + "/api/search?folderIds=0&query=&starred=false"
-
-
 headers = {
     'content-type': "application/json",
-    'authorization': "Basic YWRtaW46cHJvbS1vcGVyYXRvcg==",
 }
 
-res = requests.get(url=GET_DASHBOARD_URL, headers=headers)
+res = requests.get(url=GET_DASHBOARD_URL, headers=headers, auth=HTTPBasicAuth(GRAFANA_USER, GRAFANA_PASSWORD))
 data = res.json()
+
+start_date = (datetime.datetime.now() - datetime.timedelta(minutes=15)).timestamp()
+end_date = datetime.datetime.now().timestamp()
 
 for dashboard in data:
     print(dashboard["url"])
     URL = BASE_URL + "/render" + dashboard['url'] + "?width=1920&height=3000&from"+str(start_date)+"&to"+str(end_date)+"&kiosk"
-    request = requests.get(url=URL, headers=headers, stream=True)
+    request = requests.get(url=URL, headers=headers, stream=True,auth=HTTPBasicAuth(GRAFANA_USER, GRAFANA_PASSWORD))
     if request.status_code == 200:
         if ' ' in dashboard["title"]:
             image_name = dashboard["title"].replace(" ", "").replace("/", "")
@@ -65,7 +68,7 @@ my_file = {
 payload = {
     "filename": "Report.pdf",
     "token": SlackToken,
-    "channels": ['#kubernetes-bot'],
+    "channels": [SlackChannels],
 }
 req = requests.post("https://slack.com/api/files.upload", params=payload, files=my_file)
 print(req.json())
